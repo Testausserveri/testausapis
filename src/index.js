6b6c8b42533db8896120add79a48d12b059ae449
Mikael <46541386+ahnl@users.noreply.github.com>
2021-12-06 22:09:40 +0200

    Add express server

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 require("dotenv").config()
 
-const { Client, Intents } = require("discord.js");
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const { Client, Intents } = require("discord.js")
+const express = require("express")
+
+const app = express()
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
 
 client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+  console.log(`Logged in as ${client.user.tag}!`)
+})
 
-client.login(process.env.DISCORD_TOKEN);
+app.get("/", (req, res) => {
+    res.end(`pong ${client.user.tag}`)
+})
+
+client.login(process.env.DISCORD_TOKEN)
+app.listen(8080)
