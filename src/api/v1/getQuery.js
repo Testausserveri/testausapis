45fbc896de8a7a2cdba773583ced5a5190480f42
Esinko <34211401+Esinko@users.noreply.github.com>
2022-01-07 22:16:28 +0200

    fix: Remove query debug log

--- src/api/v1/getQuery.js
+++ src/api/v1/getQuery.js
@@ -9,7 +9,6 @@ module.exports = function getQuery(key, url) {
         // eslint-disable-next-line no-param-reassign
         if (url.startsWith("/")) url = `http://local${url}`
         const urlConstruct = new URL(url)
-        console.debug(urlConstruct.searchParams)
         return urlConstruct.searchParams.get(key)
     } catch (e) {
         return false
