d212915cf00a3ed5784c645b788c4cfc936fdbe2
Mikael <46541386+ahnl@users.noreply.github.com>
2022-05-20 17:28:48 +0300

    Refactor database.js and init models

--- src/api/v1/database/database.js
+++ src/api/v1/database/database.js
@@ -1,20 +1,37 @@
-const { connect, connection } = require("mongoose")
+/* eslint-disable function-paren-newline */
+
+import { connect, connection, model } from "mongoose"
+
+import SchemaDataCollectionConfiguration from "./schemas/dataCollectionConfiguration"
+import SchemaMessageCount from "./schemas/messageCount"
+import SchemaProjects from "./schemas/projects"
+import SchemaUserInfo from "./schemas/userInfo"
 
 /**
- * Initialize the database
+ * Initialize a database connection
+ * @returns {mongoose.Connection}
  */
 async function init() {
     return connect(process.env.TEST_CLUSTER ?? `mongodb://${process.env.MONGODB_HOST || "testausapis_mongo"}:27017/main`)
 }
 
-module.exports = {
-    incrementMessageCount,
-    updateDataCollectionPolicy,
-    getDataCollectionConfig,
-    setUserInfo,
-    removeUserInfo,
-    getUserInfo,
-    init,
-    getMessageCount,
-    connection
+const models = {
+    DataCollectionConfiguration: model(
+        "DataCollectionConfiguration",
+        SchemaDataCollectionConfiguration
+    ),
+    MessageCount: model(
+        "MessageCount",
+        SchemaMessageCount
+    ),
+    Projects: model(
+        "Projects",
+        SchemaProjects
+    ),
+    UserInfo: model(
+        "UserInfo",
+        SchemaUserInfo
+    )
 }
+
+export default { ...models, init, connection }
