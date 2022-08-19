c5439bdb9f0e4daf88ec6352c75158a577df80c2
Antti <antti@antti.codes>
2022-08-19 19:49:26 +0300

    fix: hotfix for testaustime api error handling

--- src/utils/getCodingLeaderboard.js
+++ src/utils/getCodingLeaderboard.js
@@ -5,18 +5,23 @@ import request from "./request.js"
  * @returns {{ name: string, value: string }[]}
  */
 export default async () => {
-    const { data, status } = await request("GET", "https://api.testaustime.fi/leaderboards/balls", {
-        Authorization: `Bearer ${process.env.TESTAUSTIME_TOKEN}`
-    })
-    if (status !== 200) throw new Error(`Failed to fetch leaderboard from Testaustime: ${JSON.stringify(data)}`)
-    const { members } = JSON.parse(data)
-    const leaderboard = members
-        .map((member) => ({
-            name: member.username,
-            value: member.time_coded
-        }))
-        .sort((a, b) => (b.value - a.value))
-        .slice(0, 5)
+    try {
+        const { data, status } = await request("GET", "https://api.testaustime.fi/leaderboards/balls", {
+            Authorization: `Bearer ${process.env.TESTAUSTIME_TOKEN}`
+        })
+        if (status !== 200) throw new Error(`Failed to fetch leaderboard from Testaustime: ${JSON.stringify(data)}`)
+        const { members } = JSON.parse(data)
+        const leaderboard = members
+            .map((member) => ({
+                name: member.username,
+                value: member.time_coded
+            }))
+            .sort((a, b) => (b.value - a.value))
+            .slice(0, 5)
 
-    return leaderboard
+        return leaderboard
+    } catch (error) {
+        console.error(error)
+        return []
+    }
 }
